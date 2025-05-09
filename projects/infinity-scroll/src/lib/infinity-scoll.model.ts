c8d9f90da5ce4ecef7e01f9ee73ed240408fb146
hellcat29A <bhiri22@gmail.com>
2025-05-09 22:41:19 +0100

    new-v

---
+++ projects/infinity-scroll/src/lib/infinity-scoll.model.ts
@@ -0,0 +1,9 @@
+export interface InfinityScrollConfig {
+  speed?: number;
+  hoverSpeed?: number;
+  pauseOnHover?: boolean;
+  rtl?: boolean;
+  duplicate?: boolean;
+  trackClass?: string;
+  wrapperClass?: string;
+}
